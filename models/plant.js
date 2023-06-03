6693230c89a9502e3ee41432267bbc5b398b0cb8
CurrentlyBobe <walshbobby5@gmail.com>
2023-06-03 18:43:42 -0500

    fix association bug

--- models/plant.js
+++ models/plant.js
@@ -8,7 +8,7 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      Plant.hasMany(models.Profile, { foreignKey: "profileId" });
+      Plant.belongsTo(models.Profile, { foreignKey: "profileId" });
     }
   }
   Plant.init(
--- models/profile.js
+++ models/profile.js
@@ -10,6 +10,10 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       Profile.belongsTo(models.User, { foreignKey: 'userId' })
+      Profile.hasMany(models.Plant, {
+        as: 'plantsCreated',
+        foreignKey: 'profileId',
+      })
     }
   }
 
